51664ee79bbd8b00642c51281660348e9a4a1ee0
uriasvitor <81401104+uriasvitor@users.noreply.github.com>
2024-06-03 12:16:04 -0300

    Update middleware.py

--- walldreams/src/auth/middleware.py
+++ walldreams/src/auth/middleware.py
@@ -3,7 +3,6 @@ from fastapi import Request, HTTPException, FastAPI
 from starlette.status import HTTP_403_FORBIDDEN
 from fastapi.middleware.cors import CORSMiddleware
 
-# Defina a chave API válida
 VALID_API_KEYS = ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"]
 
 async def api_key_middleware(request: Request, call_next):
